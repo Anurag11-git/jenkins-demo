cadc1a101d7011c4007ef8c115c2b872a2d44e52
Anurag11-git <anuragguhat@gmail.com>
2026-03-09 12:14:44 +0530

    code changes made

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -2,9 +2,11 @@ package com.git;
 
 public class Demo {
 	public static void main(String[] args) {
-		int a = 20;
+		int a = 30;
 		int b = 20;
 		int sum = a+b;
+		int sub = a-b;
 		System.out.println(sum);
+		System.out.println(sub);
 	}
 }
